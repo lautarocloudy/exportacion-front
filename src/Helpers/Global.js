0389029b36c8b3aa5daef1757a424ce1e7dc9e91
unknown <lautarorcloudy@gmail.com>
2024-08-19 17:34:35 -0300

    update

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
     // url: "http://localhost:3000/api/"
-    url:"https://exportacion-back.vercel.app/"
+    url:"https://exportacion-back.vercel.app/api/"
    }
    
    export default Global
